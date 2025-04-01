5ce86154768d2054765c11e9a7b02e7c7ab8e992
LuizLeineker <Luiz.Hoffmann.Leineker@gmail.com>
2025-04-01 20:03:37 -0300

    update

--- Cript/sha256.py
+++ Cript/sha256.py
@@ -1,12 +1,70 @@
-string = input("Informe uma string: ")
-char = list(string)
-
-
 ## while até 256, até quando tiver o digitos inseridor ele armazena em uma lista esses digitios
-# dps que a palavra acabar armazena 1 e depois 00 até o final
+# dps que a palavra acaba armazena 1 e depois 00 até o final
 # cada letra é referente a 8 bits, quando acabar a palavra preenche com 10000000
 # os bits final eu num lembro
 # segundo o site é isso ai Append the original message length (100000, 32 in decimal) at the end of the message block as a 64-bit big-endian integer
 # Bits `0` são adicionados até que o comprimento seja 448 bits módulo 512.
 # Os últimos 64 bits armazenam o tamanho original da mensagem em bits
+
+bits = []       # SALVA OS BITS INICIAL DA STRING
+padding = []    # SALVA OS BITS DE 0 A 448
+tamanho = []
+h0 = 0x6a09e667
+h1 = 0xbb67ae85
+h2 = 0x3c6ef372
+h3 = 0xa54ff53a
+h4 = 0x510e527f
+h5 = 0x9b05688c
+h6 = 0x1f83d9ab
+h7 = 0x5be0cd19
+
+string = input("Informe uma string: ")
+char = list(string)
+
+for i in char:
+    number = ord(i)
+    for k in range(8):
+        a = number % 2
+        number = number / 2
+        bits.append(int(a))
+
+bits.reverse()
+i = 0
+
+## DO BITS 0 ATÉ O 448
+while i in range(448):
+    if i == len(bits):
+        padding.append(1)
+    elif i < len(bits):
+        padding.append(bits[i])
+    else:
+        padding.append(0)
+    i+= 1
+
+i = 0
+
+palavra = len(char) * 8
+
+## TRASFORMA O TAMANHO DA PALAVRA EM BITS
+for i in range(8):
+    a = palavra % 2
+    palavra = palavra / 2
+    tamanho.append(int(a))
+    i+= 1
+tamanho.reverse()
+
+i = 0
+## JUNTA O TAMANHO DA PALAVRA MAIS OS 448
+while i in range(64):
+    if i < len(tamanho):
+        padding.append(tamanho[i])
+    else:
+        padding.append(0)
+    i += 1
+
+#print(padding)
+#print(len(padding))
+
+## 16 PALAVRAS DE 32 BITS, 16 * 32 = 512
+## TEMOS 64 PARES DE 8 BITS
 
